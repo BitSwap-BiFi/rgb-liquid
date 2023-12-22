2e7ce91b6df4ed2d6a63ea7964bd46e4719cfaf0
Rsync⚡️ <135646455+Rsync25@users.noreply.github.com>
2023-12-22 07:49:01 -0300

    Update poc.rs

--- poc.rs
+++ poc.rs
@@ -4,6 +4,8 @@ use rgb_core::Chain::Liquid;
 use rgb_core::Contract::Anchor::Liquid;
 use rgb_core::Contract::Anchor::Bitcoin;
 
+// Implementation for Liquid
 impl liquid 
 
+// Implementation for RGB
 impl rgb
